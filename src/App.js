ebabfdd9ff5457e7ca67493473d51929062bc410
thiago <thiago.heilmann@hotmail.com>
2021-09-02 16:21:58 -0300

    adicionado imagem e link da sgurd

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <header>
-        <div className="header-sgurd">
-          <h1>SGURD PRODUCTIONS</h1>
-        </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={sgurd.png} className="App-logo" alt="LOGO DA SGURD" />
+        <p>
+          SEJA BEM VINDO
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/channel/UCLmj9Y-YTTftS9vdxZhuy3Q"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CANAL DA SGURD
+        </a>
       </header>
     </div>
   );
