0e2e3dbd32c07c0b4eff0c5c128e158f65a4b9ec
thiago <thiago.heilmann@hotmail.com>
2021-09-03 09:35:55 -0300

    estilizacao e implementacao footer/main

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+  <div className="App">
       <header class="App-header">
         <img src={logo} className="App-logo" alt="logo da sgurd"/>
-        <p>
+        <p class="header-p">
           SEJA BEM VINDO.
         </p>
 
       </header>
-
+  <div class="main">
     <div className="oqe">
       
       <div className="h1oqe">
@@ -22,10 +22,9 @@ function App() {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet, erat a consectetur facilisis, felis lorem sollicitudin quam, ut consectetur libero nisl eget elit. Vestibulum felis ex, commodo ac nibh eget, finibus dignissim massa. Integer hendrerit lacus quis mi sagittis, nec consequat risus malesuada. Nullam et turpis vitae ex lacinia hendrerit. Nulla tincidunt ex a fermentum consectetur. Sed quis facilisis justo, id rhoncus nulla. Sed tristique fermentum nisi in porta. Cras rhoncus egestas nulla, ut auctor purus tempor eget.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet, erat a consectetur facilisis, felis lorem sollicitudin quam, ut consectetur libero nisl eget elit. Vestibulum felis ex, commodo ac nibh eget, finibus dignissim massa. Integer hendrerit lacus quis mi sagittis, nec consequat risus malesuada. Nullam et turpis vitae ex lacinia hendrerit. Nulla tincidunt ex a fermentum consectetur. Sed quis facilisis justo, id rhoncus nulla. Sed tristique fermentum nisi in porta. Cras rhoncus egestas nulla, ut auctor purus tempor eget.</p>
       </div>
-    </div>
-    <div>
-    <a
-          className="App-link"
+      <div className="botao-youtube">
+    <a class="link"
+          
           href="https://www.youtube.com/channel/UCLmj9Y-YTTftS9vdxZhuy3Q"
           target="_blank"
           rel="noopener noreferrer"
@@ -33,11 +32,21 @@ function App() {
           CANAL DA SGURD
         </a>
     </div>
-
+    </div>
+    
 
 
 
     </div>
+
+    <div class="footer">
+      <h2>CONTATO</h2>
+      <div class="div-contato">
+        <h3>sgurdproductions@gmail.com</h3>
+        <h3>(51) 99765-4234</h3>
+      </div>
+    </div>
+  </div>  
   );
 }
 
