e3bad4da86680773773f64dad3a8d834704265c0
thiagoheil <thiago.heilmann@hotmail.com>
2021-09-02 23:26:53 -0300

    implementação 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import logo from './sgurd.png';
+import logo from './img/sgurd2.jpg';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header class="App-header">
         <img src={logo} className="App-logo" alt="logo da sgurd"/>
         <p>
           SEJA BEM VINDO.
         </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/channel/UCLmj9Y-YTTftS9vdxZhuy3Q"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          CANAL DA SGURD
-        </a>
+
       </header>
 
     <div className="oqe">
+      
       <div className="h1oqe">
         <h1> O que é a SGURD?</h1>
       </div>
@@ -28,6 +22,16 @@ function App() {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet, erat a consectetur facilisis, felis lorem sollicitudin quam, ut consectetur libero nisl eget elit. Vestibulum felis ex, commodo ac nibh eget, finibus dignissim massa. Integer hendrerit lacus quis mi sagittis, nec consequat risus malesuada. Nullam et turpis vitae ex lacinia hendrerit. Nulla tincidunt ex a fermentum consectetur. Sed quis facilisis justo, id rhoncus nulla. Sed tristique fermentum nisi in porta. Cras rhoncus egestas nulla, ut auctor purus tempor eget.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet, erat a consectetur facilisis, felis lorem sollicitudin quam, ut consectetur libero nisl eget elit. Vestibulum felis ex, commodo ac nibh eget, finibus dignissim massa. Integer hendrerit lacus quis mi sagittis, nec consequat risus malesuada. Nullam et turpis vitae ex lacinia hendrerit. Nulla tincidunt ex a fermentum consectetur. Sed quis facilisis justo, id rhoncus nulla. Sed tristique fermentum nisi in porta. Cras rhoncus egestas nulla, ut auctor purus tempor eget.</p>
       </div>
+    </div>
+    <div>
+    <a
+          className="App-link"
+          href="https://www.youtube.com/channel/UCLmj9Y-YTTftS9vdxZhuy3Q"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CANAL DA SGURD
+        </a>
     </div>
 
 
